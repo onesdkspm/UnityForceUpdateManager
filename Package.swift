bfbc13a4debd1388c9765ca03f83fa802b8b630f
m2构造机 <zhanggaoqi@aobi.com>
2026-08-25 11:53:50 +0800

    [Branch Release] v1.0.0-dev-1513762 - develop
    - 更新所有 XCFrameworks 到 1.0.0-dev-1513762
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -34,8 +34,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "UnityForceUpdateManager",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityForceUpdateManager/1.0.0-dev-1511948/UnityForceUpdateManager.xcframework.zip",
-            checksum: "0d7fc55b1f019099af727d724c5f639e5acd78ce355d0b354719c9e56614996a"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityForceUpdateManager/1.0.0-dev-1513762/UnityForceUpdateManager.xcframework.zip",
+            checksum: "ed264c2e0166eb13aff3c3be5b49104bdd99737ec1f94d3b70b2c2d39a190fce"
         )
     ]
 )
